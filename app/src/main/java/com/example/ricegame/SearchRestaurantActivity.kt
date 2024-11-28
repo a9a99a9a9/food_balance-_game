d76a8eb752ff421c44dc1791effb0d28e4da01d0
main <PC@MacBook-Air-2.local>
2024-11-28 16:11:34 +0900

    search ui 기능 추가

--- app/src/main/java/com/example/ricegame/SearchRestaurantActivity.kt
+++ app/src/main/java/com/example/ricegame/SearchRestaurantActivity.kt
@@ -2,11 +2,12 @@ package com.example.ricegame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ricegame.R
 
 class SearchRestaurantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_restaurant) // 식당 검색 화면 연결
+        setContentView(R.layout.activity_search_restaurant) // XML 파일 연결
+
+        // UI 컴포넌트 초기화 (현재 데이터와 로직 없음)
     }
 }
